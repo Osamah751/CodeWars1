9c5240e1a4233d485db1bf59434ce82136d0c219
Osamah Sarraj <osamah.sarraj@gmail.com>
2019-11-16 04:40:05 -0500

    Completed Function

--- Mini Programs/Fundamentals Functions/Kyu 7/Find The Stray Number.py
+++ Mini Programs/Fundamentals Functions/Kyu 7/Find The Stray Number.py
@@ -14,3 +14,29 @@ Examples
 [17, 17, 3, 17, 17, 17, 17] ==> 3
 '''
 
+def stray(arr):
+    '''
+    a function that finds the stray number in a list
+    :param arr: a list of identical numbers except one number
+    :return: the number only used ones in the list
+    Time: O(1)
+    '''
+    # sort the numbers to make the stray number either at the beginning or the end of the array
+    arr.sort()
+
+    # if the first number is the same as the second then the first number is the stray number(after sorting)
+    # otherwise the last number is the stray number
+    if arr[0] != arr[1]:
+        return arr[0]
+    else:
+        return arr[-1]
+
+
+# Test Case #1: (2)
+print(stray([1, 1, 1, 1, 1, 1, 2]))
+
+# Test Case #2: (3)
+print(stray([2, 3, 2, 2, 2]))
+
+# Test Case #3: (3)
+print(stray([3, 2, 2, 2, 2]))
