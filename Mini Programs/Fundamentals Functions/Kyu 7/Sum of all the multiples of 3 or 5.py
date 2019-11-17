e41836feef72f9127eb84be35a487bd59e4009cc
Osamah Sarraj <osamah.sarraj@gmail.com>
2019-11-17 00:11:44 -0500

    create (Not_Efficient) folder setting up a new project

---
+++ Mini Programs/Fundamentals Functions/Kyu 7/Sum of all the multiples of 3 or 5.py
@@ -0,0 +1,25 @@
+'''
+7 kyu
+Sum of all the multiples of 3 or 5
+
+Your task is to write function findSum.
+
+Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+For example:
+
+findSum(5) should return 8 (3 + 5)
+
+findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+'''
+
+
+def find(n):
+    pass
+
+
+# Test Case #1: (8)
+print(find(5))
+
+# Test Case #2: (33)
+print(find(10))
