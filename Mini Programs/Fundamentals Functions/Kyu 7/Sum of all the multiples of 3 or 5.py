686c7f09b8c7b90a086d62bbfabaf3682956968d
Osamah Sarraj <osamah.sarraj@gmail.com>
2019-11-17 00:24:09 -0500

    workable function

--- Mini Programs/Fundamentals Functions/Kyu 7/Sum of all the multiples of 3 or 5.py
+++ Mini Programs/Fundamentals Functions/Kyu 7/Sum of all the multiples of 3 or 5.py
@@ -15,7 +15,14 @@ findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
 
 
 def find(n):
-    pass
+    # set a place to add the founded numbers
+    sum = 0
+    # try all the number from 1 to (n)
+    for i in range(1, n+1):
+        # if they are either divisible by 3 or 5 add it to sum
+        if i%3==0 or i%5==0:
+            sum += i
+    return sum
 
 
 # Test Case #1: (8)
