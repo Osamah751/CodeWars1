f3a322d850ad0d2bf8ac4b103dfc004c54148157
Osamah Sarraj <osamah.sarraj@gmail.com>
2022-12-25 14:56:40 +0300

    Create Convert a Boolean to a String.py

---
+++ Python/Kyu 8/Convert a Boolean to a String.py
@@ -0,0 +1,19 @@
+"""
+8 kyu
+Convert a Boolean to a String
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+"""
+
+
+def boolean_to_string(b):
+    return str(b)
+
+
+# Test Case #1: "True"
+print(boolean_to_string(True))
+
+# Test Case #2: "False"
+print(boolean_to_string(False))
